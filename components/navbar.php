12fd86665c9c9fb8425e0614583ba604469449cb
Gafisk <galihrestubaihq@gmail.com>
2025-01-27 16:39:21 +0700

    Update Pertama

---
+++ components/navbar.php
@@ -0,0 +1,34 @@
+<a href="index.html" class="logo d-flex align-items-center">
+  <!-- Uncomment the line below if you also wish to use an image logo -->
+  <!-- <img src="assets/img/logo.png" alt=""> -->
+  <h1 class="sitename">BU Malang</h1>
+</a>
+<nav id="navmenu" class="navmenu">
+  <ul>
+    <li><a href="#" class="active">Beranda</a></li>
+    <li><a href="#">Berita</a></li>
+    <li class="dropdown"><a href="#"><span>Profil</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+      <ul>
+        <li><a href="#">Sejarah</a></li>
+        <li><a href="#">Struktur Organisasi</a></li>
+        <li><a href="#">Makna dan Lambang</a></li>
+        <li><a href="#">Visi dan Misi</a></li>
+        <li><a href="#">Program Kerja</a></li>
+      </ul>
+    </li>
+    <li class="dropdown"><a href="#"><span>Aktivitas</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+      <ul>
+        <li><a href="#">Prestasi Awardee</a></li>
+        <li><a href="#">Publikasi Awardee</a></li>
+      </ul>
+    </li>
+    <li class="dropdown"><a href="#"><span>Pelaporan</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+      <ul>
+        <li><a href="#">Pelaporan Awardee Baru</a></li>
+        <li><a href="#">Pelaporan Alumni</a></li>
+      </ul>
+    </li>
+    <li><a href="#contact">Kontak</a></li>
+  </ul>
+  <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+</nav>
