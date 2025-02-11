247138cbb8eeb965bf88dd42e1bf798a53b1cad5
Gafisk <galihrestubaihq@gmail.com>
2025-02-11 10:50:14 +0700

    update dashboard admin

--- adminbu/index.php
+++ adminbu/index.php
@@ -27,6 +27,36 @@ if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) >
 
 // Update last activity timestamp
 $_SESSION['last_activity'] = time();
+
+include '../connections/conn.php';
+
+// Array berisi query untuk setiap tabel
+$queries = [
+  'anggota' => "SELECT COUNT(*) AS total FROM anggota_divisi",
+  'divisi' => "SELECT COUNT(*) AS total FROM divisi",
+  'berita' => "SELECT COUNT(*) AS total FROM berita",
+  'event' => "SELECT COUNT(*) AS total FROM event",
+  'kampus_awardee' => "SELECT COUNT(*) AS total FROM kampus_awardee",
+  'prestasi' => "SELECT COUNT(*) AS total FROM prestasi",
+  'publikasi' => "SELECT COUNT(*) AS total FROM publikasi",
+];
+
+// Array untuk menyimpan hasil
+$results = [];
+
+// Loop untuk menjalankan setiap query dan simpan hasilnya
+foreach ($queries as $key => $query) {
+  $result = mysqli_query($conn, $query);
+  if ($result) {
+    $data = mysqli_fetch_assoc($result);
+    $results[$key] = $data['total'];
+  } else {
+    $results[$key] = "Error: " . mysqli_error($conn);
+  }
+}
+
+// Tutup koneksi
+mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
@@ -97,24 +127,14 @@ $_SESSION['last_activity'] = time();
           <!--begin::Row-->
           <div class="row">
             <!--begin::Col-->
-            <div class="col-lg-3 col-6">
+            <div class="col-lg-4 col-6">
               <!--begin::Small Box Widget 1-->
               <div class="small-box text-bg-primary">
                 <div class="inner">
-                  <h3>150</h3>
-                  <p>New Orders</p>
+                  <h3><?= $results['kampus_awardee'] ?></h3>
+                  <p>Kampus Awardee</p>
                 </div>
-                <svg
-                  class="small-box-icon"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true">
-                  <path
-                    d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"></path>
-                </svg>
-                <a
-                  href="#"
+                <a href="pages/kelola-stats/daftar-kampus-awardee.php"
                   class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
                   More info <i class="bi bi-link-45deg"></i>
                 </a>
@@ -122,22 +142,13 @@ $_SESSION['last_activity'] = time();
               <!--end::Small Box Widget 1-->
             </div>
             <!--end::Col-->
-            <div class="col-lg-3 col-6">
+            <div class="col-lg-4 col-6">
               <!--begin::Small Box Widget 2-->
               <div class="small-box text-bg-success">
                 <div class="inner">
-                  <h3>53<sup class="fs-5">%</sup></h3>
-                  <p>Bounce Rate</p>
+                  <h3><?= $results['divisi'] ?></h3>
+                  <p>Jumlah Divisi</p>
                 </div>
-                <svg
-                  class="small-box-icon"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true">
-                  <path
-                    d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z"></path>
-                </svg>
                 <a
                   href="#"
                   class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
@@ -147,60 +158,20 @@ $_SESSION['last_activity'] = time();
               <!--end::Small Box Widget 2-->
             </div>
             <!--end::Col-->
-            <div class="col-lg-3 col-6">
+            <div class="col-lg-4 col-12">
               <!--begin::Small Box Widget 3-->
               <div class="small-box text-bg-warning">
                 <div class="inner">
-                  <h3>44</h3>
-                  <p>User Registrations</p>
+                  <h3><?= $results['anggota'] ?></h3>
+                  <p>Jumlah Pengurus Forum</p>
                 </div>
-                <svg
-                  class="small-box-icon"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true">
-                  <path
-                    d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
-                </svg>
                 <a
-                  href="#"
+                  href="pages/kelola-profil/daftar-profil-pengurus.php"
                   class="small-box-footer link-dark link-underline-opacity-0 link-underline-opacity-50-hover">
                   More info <i class="bi bi-link-45deg"></i>
                 </a>
               </div>
               <!--end::Small Box Widget 3-->
-            </div>
-            <!--end::Col-->
-            <div class="col-lg-3 col-6">
-              <!--begin::Small Box Widget 4-->
-              <div class="small-box text-bg-danger">
-                <div class="inner">
-                  <h3>65</h3>
-                  <p>Unique Visitors</p>
-                </div>
-                <svg
-                  class="small-box-icon"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true">
-                  <path
-                    clip-rule="evenodd"
-                    fill-rule="evenodd"
-                    d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"></path>
-                  <path
-                    clip-rule="evenodd"
-                    fill-rule="evenodd"
-                    d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"></path>
-                </svg>
-                <a
-                  href="#"
-                  class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                  More info <i class="bi bi-link-45deg"></i>
-                </a>
-              </div>
-              <!--end::Small Box Widget 4-->
             </div>
             <!--end::Col-->
           </div>
@@ -213,20 +184,11 @@ $_SESSION['last_activity'] = time();
               <!--begin::Small Box Widget 1-->
               <div class="small-box text-bg-primary">
                 <div class="inner">
-                  <h3>150</h3>
-                  <p>New Orders</p>
+                  <h3><?= $results['berita'] ?></h3>
+                  <p>Jumlah Berita</p>
                 </div>
-                <svg
-                  class="small-box-icon"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true">
-                  <path
-                    d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"></path>
-                </svg>
                 <a
-                  href="#"
+                  href="pages/berita/daftar-berita.php"
                   class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
                   More info <i class="bi bi-link-45deg"></i>
                 </a>
@@ -238,20 +200,11 @@ $_SESSION['last_activity'] = time();
               <!--begin::Small Box Widget 2-->
               <div class="small-box text-bg-success">
                 <div class="inner">
-                  <h3>53<sup class="fs-5">%</sup></h3>
-                  <p>Bounce Rate</p>
+                  <h3><?= $results['event'] ?></h3>
+                  <p>Jumlah Event</p>
                 </div>
-                <svg
-                  class="small-box-icon"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true">
-                  <path
-                    d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z"></path>
-                </svg>
                 <a
-                  href="#"
+                  href="pages/kelola-aktivitas/daftar-events.php"
                   class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
                   More info <i class="bi bi-link-45deg"></i>
                 </a>
@@ -263,20 +216,11 @@ $_SESSION['last_activity'] = time();
               <!--begin::Small Box Widget 3-->
               <div class="small-box text-bg-warning">
                 <div class="inner">
-                  <h3>44</h3>
-                  <p>User Registrations</p>
+                  <h3><?= $results['prestasi'] ?></h3>
+                  <p>Jumlah Prestasi</p>
                 </div>
-                <svg
-                  class="small-box-icon"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true">
-                  <path
-                    d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
-                </svg>
                 <a
-                  href="#"
+                  href="pages/kelola-aktivitas/daftar-prestasi.php"
                   class="small-box-footer link-dark link-underline-opacity-0 link-underline-opacity-50-hover">
                   More info <i class="bi bi-link-45deg"></i>
                 </a>
@@ -288,26 +232,11 @@ $_SESSION['last_activity'] = time();
               <!--begin::Small Box Widget 4-->
               <div class="small-box text-bg-danger">
                 <div class="inner">
-                  <h3>65</h3>
-                  <p>Unique Visitors</p>
+                  <h3><?= $results['publikasi'] ?></h3>
+                  <p>Jumlah Publikasi</p>
                 </div>
-                <svg
-                  class="small-box-icon"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true">
-                  <path
-                    clip-rule="evenodd"
-                    fill-rule="evenodd"
-                    d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"></path>
-                  <path
-                    clip-rule="evenodd"
-                    fill-rule="evenodd"
-                    d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"></path>
-                </svg>
                 <a
-                  href="#"
+                  href="pages/kelola-aktivitas/daftar-publikasi.php"
                   class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
                   More info <i class="bi bi-link-45deg"></i>
                 </a>
